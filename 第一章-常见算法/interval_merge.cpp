2d41bff94fd79f9fceb6da3d05d22c98598c4f8f
HuichiZhou <133013428+HuichiZhou@users.noreply.github.com>
2023-05-20 20:17:22 +0800

    Add files via upload

---
+++ 第一章-常见算法/interval_merge.cpp
@@ -0,0 +1,50 @@
+// 按区间左端点进行排序
+// 
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+const int N = 100010;
+typedef pair<int, int> PII;
+int n;
+vector<PII> segs;
+
+
+void merge(vector<PII> &segs)
+{
+    vector<PII> res;
+
+    sort(segs.begin(), segs.end());
+
+    int st = -2e9, ed = -2e9;
+    for (auto seg : segs)
+        if (ed < seg.first)
+        {
+            if (st != -2e9) {
+                res.push_back({st, ed});
+            }
+            st = seg.first, ed = seg.second;
+        }
+        else ed = max(ed, seg.second);
+
+    if (st != -2e9) res.push_back({st, ed});
+
+    segs = res;
+}
+
+
+
+int main(){
+    cin>>n;
+    for(int i=0; i<n; i++){
+        int l, r;
+        cin>>l>>r;
+        segs.push_back({l, r}); 
+    }
+    merge(segs);
+    
+    cout<<segs.size()<<endl;
+    return 0;
+}
